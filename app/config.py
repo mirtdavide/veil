fe70e62bce3d66e33c4f9598bf9d0194d3c5f106
mirtdavide <mirtdavide@gmail.com>
2026-07-14 19:14:26 +0300

    feat: add Alembic migrations and create initial schema

--- app/config.py
+++ app/config.py
@@ -4,9 +4,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
 
-    model_config = SettingsConfigDict(env_file=".env") #Read from .env
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore") #Read from .env
     app_name: str = "Veil"
     debug: bool = False
     secret_key: str
+    database_url: str
 
 settings = Settings() #We create an instance here and import it along the project
